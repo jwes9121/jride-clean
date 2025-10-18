462c4209f2b19660538b61cdfa92f60816236ae3
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-10-18 08:55:01 +0800

    Update and rename next.config.js to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // add any options you actually need here
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
