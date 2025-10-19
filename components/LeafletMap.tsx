1160585308e2cb2dcb0bdd7eefc44272cb299496
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-10-19 14:41:27 +0800

    Update LeafletMap.tsx

--- components/LeafletMap.tsx
+++ components/LeafletMap.tsx
@@ -1,52 +1,18 @@
 "use client";
 
-import React from "react";
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  type MapContainerProps,
-} from "react-leaflet";
-import type { LatLngExpression } from "leaflet";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
+import dynamic from "next/dynamic";
 
-// Default icon fix so markers show up in Next.js bundles
-const defaultIcon = L.icon({
-  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  iconRetinaUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
+// Load the real map client-side only
+const LeafletMapInner = dynamic(() => import("./LeafletMapInner"), {
+  ssr: false,
+  // optional loading fallback
+  loading: () => <div style={{height: 300}}>Loading map…</div>,
 });
-L.Marker.prototype.options.icon = defaultIcon;
 
-type Props = MapContainerProps & {
-  markerAt?: LatLngExpression;
-  showPopupText?: string;
-};
-
-export default function LeafletMap({
-  markerAt,
-  showPopupText = "Center",
-  children,
-  ...props
-}: Props) {
-  return (
-    <MapContainer {...props}>
-      {/* OSM tiles by default; swap to Mapbox if you like */}
-      <TileLayer
-        url={`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`}
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>'
-      />
-      {markerAt && (
-        <Marker position={markerAt as [number, number]}>
-          <Popup>{showPopupText}</Popup>
-        </Marker>
-      )}
-      {children}
-    </MapContainer>
-  );
+export default function LeafletMap(props: {
+  center?: [number, number];
+  zoom?: number;
+  height?: number | string;
+}) {
+  return <LeafletMapInner {...props} />;
 }
