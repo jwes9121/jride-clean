8333459b351b8cad0cc247f905f0d74de65d34b4
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-10-20 06:50:20 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,9 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  // protect everything except Next.js assets and NextAuth endpoints
   matcher: [
+    // protect real pages, but don't run on next assets or auth endpoints
     "/((?!_next/static|_next/image|favicon.ico|api/auth|api/_diag).*)",
   ],
 };
+
