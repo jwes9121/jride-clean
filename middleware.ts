581602e1a35ed171427fac84dcd92e4272fbc820
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-10-23 01:08:04 +0800

    Update and rename middleware.off.ts to middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
-// middleware.ts
 export { auth as middleware } from "@/auth";
 
 export const config = {
-  // Only protect real app areas; DO NOT match /auth or /api/auth
   matcher: [
     "/driver/:path*",
     "/rider/:path*",
