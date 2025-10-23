514755bae262f5e7856e907b4061966413b59985
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-10-23 17:37:55 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
+  cacheHandler: false,
+  experimental: { staleTimes: { dynamic: 0 } },
+  headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Cache-Control", value: "no-store, max-age=0" },
+          { key: "Pragma", value: "no-cache" },
+          { key: "Expires", value: "0" }
+        ]
+      }
+    ];
+  },
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "lh3.googleusercontent.com" }
+    ]
+  }
 };
 
 export default nextConfig;
-
