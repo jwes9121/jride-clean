a2d4298211a21dc09987c8fe738ecc6c3dd5d81f
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-10-24 08:52:36 +0800

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
-// app/api/auth/[...nextauth]/route.ts
+[System.IO.Directory]::CreateDirectory("$root\app\api\auth\[...nextauth]") | Out-Null
+$route = @'
 export { GET, POST } from "../../../auth-impl";
-
+'@
+[System.IO.File]::WriteAllText("$root\app\api\auth\[...nextauth]\route.ts", $route, $utf8)
