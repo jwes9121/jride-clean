310a62b83346529daa63adab098ff2dcc597cc87
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-10-27 19:38:02 +0800

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,6 @@
 
 // We are five levels deep under /app,
 // so we need "../../../../../auth.config" to reach the root-level auth.config.ts.
-// DO NOT shorten this. DO NOT point to "app/auth" or "auth/".
 import { handlers } from "../../../../../auth.config";
 
 export const { GET, POST } = handlers;
