3879733014d4e5afa625a8a8e351b832a11dcbb0
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-10-28 06:13:05 +0800

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,6 @@
+@'
 import { handlers } from "@/configs/nextauth";
 
 export const { GET, POST } = handlers;
+'@ | Set-Content "C:\Users\jwes9\Desktop\jride-clean\app\api\auth\[...nextauth]\route.ts"
+
