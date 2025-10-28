7764d8b1cd1829203283a390938d7bcf2d7beb64
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-10-28 12:26:27 +0800

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,4 @@
 export const runtime = "nodejs";
 
-// Re-export GET and POST directly from auth.ts.
-// This gives NextAuth full control of /api/auth/* including /signin, /callback, /error etc.
 export { GET, POST } from "../../../../auth";
+
