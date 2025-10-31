4c93f53b56231c4b80608a105a46797d13d286dc
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-10-31 12:39:54 +0800

    Update mapbox-gl-csp-worker.d.ts

--- types/mapbox-gl-csp-worker.d.ts
+++ types/mapbox-gl-csp-worker.d.ts
@@ -1,9 +1,11 @@
-declare module 'mapbox-gl/dist/mapbox-gl-csp' {
-  import mapboxgl from 'mapbox-gl';
+// Re-export all types from the official typings so TS knows about Map, Marker, etc.
+declare module "mapbox-gl/dist/mapbox-gl-csp" {
+  import mapboxgl from "mapbox-gl";
+  export * from "mapbox-gl";
   export default mapboxgl;
 }
 
-declare module 'mapbox-gl/dist/mapbox-gl-csp-worker' {
+declare module "mapbox-gl/dist/mapbox-gl-csp-worker" {
   const WorkerFactory: { new (): Worker; default: new () => Worker };
   export default WorkerFactory;
 }
