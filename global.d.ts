6b0c7a61c831bb8451859a16a6090dea502d2d2e
Jwes <jwes9121@users.noreply.github.com>
2025-10-31 12:22:52 +0800

    fix(types): add global.d.ts for mapbox-gl + include in tsconfig (Vercel build)

---
+++ global.d.ts
@@ -0,0 +1,8 @@
+﻿declare module "mapbox-gl" {
+  const mapboxgl: any;
+  export default mapboxgl;
+}
+declare module "mapbox-gl/dist/mapbox-gl-csp-worker" {
+  const WorkerClass: any;
+  export default WorkerClass;
+}
