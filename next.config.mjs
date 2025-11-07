78b3df3685428f2a8c79b9338c93e4bb5289de7e
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-11-07 11:38:20 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Stop Next from passing deprecated ESLint options during build
-  eslint: { ignoreDuringBuilds: true },
-
-  // If you previously had experimental.appdir or appdir, delete it.
-  // Next 14 already uses the app router by default.
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
