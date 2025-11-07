bf4362a783e4aa66fadd14ac19f59d6645336c1c
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-11-07 12:01:14 +0800

    Delete next.config.cjs

--- next.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      { source: "/auth/callback", destination: "/auth/callback-info", permanent: true },
-    ];
-  },
-};
-module.exports = nextConfig;
