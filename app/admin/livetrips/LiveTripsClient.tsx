adfdcff7a7e9ae0438faaa8c534b74a38d1a2caf
Jwes <jwes9121@users.noreply.github.com>
2026-03-20 09:45:14 +0800

    livetrips: remove immediate refresh on visibility change

--- app/admin/livetrips/LiveTripsClient.tsx
+++ app/admin/livetrips/LiveTripsClient.tsx
@@ -340,7 +340,6 @@ export default function LiveTripsClient() {
     };
 
     const onVisibilityChange = () => {
-      refreshAllRef.current?.("visibility").catch(() => {});
       schedule();
     };
 
