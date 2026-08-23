ff8b121bca8db2465668e81cdd3aeae58c54b17b
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2026-08-23 14:59:32 +0800

    Attach vendor performance panel to the portal

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import VendorMenuEditorAssist from "./components/VendorMenuEditorAssist";
+import VendorPerformancePanel from "./components/VendorPerformancePanel";
 import VendorPresenceHeartbeat from "./components/VendorPresenceHeartbeat";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,6 +24,7 @@ export default function RootLayout({
         {children}
         <VendorMenuEditorAssist />
         <VendorPresenceHeartbeat />
+        <VendorPerformancePanel />
       </body>
     </html>
   );
