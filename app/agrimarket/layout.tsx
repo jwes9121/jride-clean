468d95ed8947e7550f25b345c5301c3cbaa0b119
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2026-09-08 05:01:53 +0800

    Attach AgriMarket native shell cleanup

--- app/agrimarket/layout.tsx
+++ app/agrimarket/layout.tsx
@@ -1,10 +1,12 @@
 import AgrimarketFarmerLoginLink from "./AgrimarketFarmerLoginLink";
+import AgrimarketNativeShellCleanup from "./AgrimarketNativeShellCleanup";
 
 export default function AgrimarketLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
       <AgrimarketFarmerLoginLink />
+      <AgrimarketNativeShellCleanup />
     </>
   );
 }
