55cf0594e7f5963207cabf55974b81986ab2d34b
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2026-09-09 06:09:46 +0800

    Enable Takeout town and store UI refinement

--- app/takeout/layout.tsx
+++ app/takeout/layout.tsx
@@ -1,5 +1,6 @@
 import TakeoutPassengerShell from "./TakeoutPassengerShell";
 import "./takeout-jride.css";
+import "./takeout-jride-v2.css";
 
 export default function TakeoutLayout({ children }: { children: React.ReactNode }) {
   return <TakeoutPassengerShell>{children}</TakeoutPassengerShell>;
