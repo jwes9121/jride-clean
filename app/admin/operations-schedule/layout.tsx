4e6918809ef3d04affdc63e4d21fd7193ced74b2
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2026-09-16 21:39:12 +0800

    Add meetings navigation to Operations Schedule layout

--- app/admin/operations-schedule/layout.tsx
+++ app/admin/operations-schedule/layout.tsx
@@ -2,7 +2,18 @@ import type { ReactNode } from "react";
 import EmployeeGpsCheck from "./EmployeeGpsCheck";
 import ScheduleCoach from "./ScheduleCoach";
 import AdminTodayRest from "./AdminTodayRest";
+import MeetingShell from "./MeetingShell";
 
 export default function OperationsScheduleLayout({ children }: { children: ReactNode }) {
-  return <EmployeeGpsCheck><>{children}<ScheduleCoach /><AdminTodayRest /></></EmployeeGpsCheck>;
+  return (
+    <EmployeeGpsCheck>
+      <MeetingShell>
+        <>
+          {children}
+          <ScheduleCoach />
+          <AdminTodayRest />
+        </>
+      </MeetingShell>
+    </EmployeeGpsCheck>
+  );
 }
